5662c0600afa9e606ddeaceecce7dab5b6cfb82e
nishanmazumder <arosh019@gmail.com>
2022-07-19 15:23:47 +0600

    checkout running

--- functions.php
+++ functions.php
@@ -59,3 +59,58 @@ require_once NM_DIR_PATH . '/inc/template-tags.php';
 
 // Ship to different address - UNCHECKED
 // add_filter( 'woocommerce_ship_to_different_address_checked', '__return_false' );
+
+// Seperate Payment area from checkout
+remove_action('woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
+add_action('woocommerce_checkout_payment_hook', 'woocommerce_checkout_payment', 10 );
+
+add_action('woocommerce_after_checkout_form', 'nm_payment_method');
+
+function nm_payment_method(){
+  do_action('woocommerce_checkout_payment_hook');
+}
+
+// Change Place Order button
+add_filter( 'woocommerce_order_button_text', 'woo_custom_order_button_text' );
+
+function woo_custom_order_button_text() {
+    return __( 'Continue Shopping', 'woocommerce' );
+}
+
+// Remove Order Notes from checkout field in Woocommerce
+add_filter( 'woocommerce_enable_order_notes_field', '__return_false', 9999 );
+
+// Remoce Privacy policy
+// if ( is_checkout() ) {
+//    remove_action('woocommerce_checkout_terms_and_conditions', 'wc_checkout_privacy_policy_text', 20);
+//    remove_action('woocommerce_checkout_terms_and_conditions', 'wc_terms_and_conditions_page_content', 30);
+// }
+
+add_action('woocommerce_checkout_before_order_review', 'nm_coupon_code_checkout');
+
+function nm_coupon_code_checkout(){
+   if (wc_coupons_enabled()) { ?>
+      <div class="nm_checkout_coupon">
+         <div class="coupon">
+            <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e('Gift card or discount code', 'woocommerce'); ?>" />
+            <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e('Apply', 'woocommerce'); ?>"><?php esc_attr_e('Apply', 'woocommerce'); ?></button>
+            <?php do_action('woocommerce_cart_coupon'); ?>
+         </div>
+      </div>
+   <?php }
+}
+
+
+// Remove default order details
+//  remove_action('woocommerce_checkout_order_review', 'woocommerce_order_review', 10);
+// add_action('nm_woocommerce_custom_details', 'woocommerce_order_review', 11 );
+// add_action('shopengine-order-review-thumbnail', 'nm_woocommerce_order_review', 11);
+
+
+// function nm_woocommerce_order_review(){
+//    echo "test";
+// }
+
+
+
+
--- woocommerce/checkout/form-billing.php
+++ woocommerce/checkout/form-billing.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Checkout billing information form
  *
@@ -16,59 +17,82 @@
  * @global WC_Checkout $checkout
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 ?>
 <div class="woocommerce-billing-fields">
-	<?php if ( wc_ship_to_billing_address_only() && WC()->cart->needs_shipping() ) : ?>
+	<div class="nm_checkout_header">
+		<?php echo get_custom_logo(); ?>
 
-		<h3><?php esc_html_e( 'Billing &amp; Shipping', 'woocommerce' ); ?></h3>
+		<ul>
+			<li>Information ></li>
+			<li>Shipping ></li>
+			<li>Payment ></li>
+			<li>Done</li>
+		</ul>
+	</div>
 
-	<?php else : ?>
+	<div class="nm_express_checkout">
+		<h3>All payment method will appear on this place...</h3>
+	</div>
 
-		<h3><?php esc_html_e( 'Billing details', 'woocommerce' ); ?></h3>
+	<div class="nm_checkout_seperator">
+		<div></div>
+		<span>OR</span>
+	</div>
 
-	<?php endif; ?>
+	<div class="nm_billing_title">
+		<?php if (wc_ship_to_billing_address_only() && WC()->cart->needs_shipping()) : ?>
 
-	<?php do_action( 'woocommerce_before_checkout_billing_form', $checkout ); ?>
+			<h3><?php esc_html_e('Billing &amp; Shipping', 'woocommerce'); ?></h3>
+
+		<?php else : ?>
+
+			<h3><?php esc_html_e('Contact information', 'woocommerce'); ?></h3> <span>Already have an account? <a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>">Log in</a></span>
+
+		<?php endif; ?>
+
+	</div>
+
+	<?php do_action('woocommerce_before_checkout_billing_form', $checkout); ?>
 
 	<div class="woocommerce-billing-fields__field-wrapper">
 		<?php
-		$fields = $checkout->get_checkout_fields( 'billing' );
+		$fields = $checkout->get_checkout_fields('billing');
 
-		foreach ( $fields as $key => $field ) {
-			woocommerce_form_field( $key, $field, $checkout->get_value( $key ) );
+		foreach ($fields as $key => $field) {
+			woocommerce_form_field($key, $field, $checkout->get_value($key));
 		}
 		?>
 	</div>
 
-	<?php do_action( 'woocommerce_after_checkout_billing_form', $checkout ); ?>
+	<?php do_action('woocommerce_after_checkout_billing_form', $checkout); ?>
 </div>
 
-<?php if ( ! is_user_logged_in() && $checkout->is_registration_enabled() ) : ?>
+<?php if (!is_user_logged_in() && $checkout->is_registration_enabled()) : ?>
 	<div class="woocommerce-account-fields">
-		<?php if ( ! $checkout->is_registration_required() ) : ?>
+		<?php if (!$checkout->is_registration_required()) : ?>
 
 			<p class="form-row form-row-wide create-account">
 				<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
-					<input class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" id="createaccount" <?php checked( ( true === $checkout->get_value( 'createaccount' ) || ( true === apply_filters( 'woocommerce_create_account_default_checked', false ) ) ), true ); ?> type="checkbox" name="createaccount" value="1" /> <span><?php esc_html_e( 'Create an account?', 'woocommerce' ); ?></span>
+					<input class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" id="createaccount" <?php checked((true === $checkout->get_value('createaccount') || (true === apply_filters('woocommerce_create_account_default_checked', false))), true); ?> type="checkbox" name="createaccount" value="1" /> <span><?php esc_html_e('Create an account?', 'woocommerce'); ?></span>
 				</label>
 			</p>
 
 		<?php endif; ?>
 
-		<?php do_action( 'woocommerce_before_checkout_registration_form', $checkout ); ?>
+		<?php do_action('woocommerce_before_checkout_registration_form', $checkout); ?>
 
-		<?php if ( $checkout->get_checkout_fields( 'account' ) ) : ?>
+		<?php if ($checkout->get_checkout_fields('account')) : ?>
 
 			<div class="create-account">
-				<?php foreach ( $checkout->get_checkout_fields( 'account' ) as $key => $field ) : ?>
-					<?php woocommerce_form_field( $key, $field, $checkout->get_value( $key ) ); ?>
+				<?php foreach ($checkout->get_checkout_fields('account') as $key => $field) : ?>
+					<?php woocommerce_form_field($key, $field, $checkout->get_value($key)); ?>
 				<?php endforeach; ?>
 				<div class="clear"></div>
 			</div>
 
 		<?php endif; ?>
 
-		<?php do_action( 'woocommerce_after_checkout_registration_form', $checkout ); ?>
+		<?php do_action('woocommerce_after_checkout_registration_form', $checkout); ?>
 	</div>
 <?php endif; ?>
